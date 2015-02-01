9dbdf4ad6a429c91a3832fc2a62bb0cb6f94798e
BestPig <bestpig82@gmail.com>
2015-02-01 03:04:41 +0100

    SystemUI: Add USB Tether tile
    Author: BestPig <bestpig82@gmail.com>
    Date:   Sun Feb 1 03:04:41 2015 +0100
    
        SystemUI: add USB Tether tile
    
         * Added vector drawables (blunden)
    
         * Long click action changed to open Tethering options, in the same way
           as Google does for Hotspot tile (bgcngm)
    
         * USB Tethering enablement check refactored (bgcngm and luca020400)
    
        Change-Id: Ic44f2b596fe32152f3f722347a855325c52113c6
    
    Author: Gabriele M <moto.falcon.git@gmail.com>
    Date:   Fri Apr 27 20:51:42 2018 +0200
    
        SystemUI: Toggle USB tethering only when USB is connected
    
        Pressing the tile when unplugged causes USB tethering to turn on
        as soon as the cable is connected. This can happen long after the
        tile has been pressed, so don't do anything if unplugged.
    
        Change-Id: Ib8bbdc758097ca7af5d4e7c3483c28ba42fb9ce1
    
    Change-Id: Ibbc0ba9de2cc3eee2a7f6a9c9f005730244f0b3a
    
    [someone5678] Forward port to QPR3
    
    shuttercat: add in UTILITIES group
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/euclid/EuclidModule.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/euclid/EuclidModule.kt
@@ -27,6 +27,7 @@ import com.android.systemui.qs.tiles.NfcTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.SoundTile
+import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.base.shared.model.QSTileConfig;
 import com.android.systemui.qs.tiles.base.shared.model.QSTilePolicy;
 import com.android.systemui.qs.tiles.base.shared.model.QSTileUIConfig;
@@ -71,6 +72,12 @@ interface EuclidModule {
     @IntoMap
     @StringKey(SoundTile.TILE_SPEC)
     fun bindSoundTile(soundTile: SoundTile): QSTileImpl<*>
+
+    /** Inject UsbTetherTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(UsbTetherTile.TILE_SPEC)
+    fun bindUsbTetherTile(usbTetherTile: UsbTetherTile): QSTileImpl<*>
 
     companion object {
         @Provides
@@ -156,6 +163,21 @@ interface EuclidModule {
                 uiConfig = QSTileUIConfig.Resource(
                     iconRes = R.drawable.ic_qs_ringer_audible,
                     labelRes = R.string.quick_settings_sound_label
+                ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.UTILITIES
+            )
+        }
+
+       @Provides
+        @IntoMap
+        @StringKey(UsbTetherTile.TILE_SPEC)
+        fun provideUsbTetherConfig(uiEventLogger: QsEventLogger): QSTileConfig {
+            return QSTileConfig(
+                tileSpec = TileSpec.create(UsbTetherTile.TILE_SPEC),
+                uiConfig = QSTileUIConfig.Resource(
+                    iconRes = R.drawable.ic_qs_usb_tether,
+                    labelRes = R.string.quick_settings_usb_tether_label
                 ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.UTILITIES
