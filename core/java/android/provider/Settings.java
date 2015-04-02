63965965bbd0fe297806ec3c0c06501a4c26b7b5
martincz <martincz@mokeedev.com>
2015-04-02 22:21:36 +0800

    PhoneWindowManager: Tap volume buttons to answer call [1/2]
    Change-Id: I5ebe1ad88950ba56ce1445b77b7f8cdf030463da
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6228,6 +6228,13 @@ public final class Settings {
          */
         @Readable
         public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+        
+        /**
+         * Whether to answer the call with the volume keys, the value is boolean.
+         * 0 = 0ff, 1 = on
+         * @hide
+         */
+        public static final String VOLUME_ANSWER_CALL = "volume_answer_call";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
