7e946147ae11156ba5fe1fa264af9d91825643ad
stofstik <verstegen.daan@gmail.com>
2015-08-17 17:42:53 +0200

    SystemUI: Add tile to show volume panel
    Squashed:
    
        From: Joey Huab <joey@evolution-x.org>
        Date: Wed, 18 Mar 2020 16:23:51 +0000
        Subject: QS: Use Settings.Panel intent for Volume Tile
    
        Signed-off-by: Joey Huab <joey@evolution-x.org>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: Ic583c24e304d1edc903127237fc9eb5c7eeb7e4d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
@@ -32,6 +32,7 @@ import com.android.systemui.qs.tiles.SoundSearchTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VpnTile
+import com.android.systemui.qs.tiles.VolumeTile
 import com.android.systemui.qs.tiles.WifiTile
 
 import dagger.Binds
@@ -131,6 +132,12 @@ interface EuclidOSModule {
     @IntoMap
     @StringKey(VpnTile.TILE_SPEC)
     fun bindVpnTile(vpnTile: VpnTile): QSTileImpl<*>
+    
+    /** Inject VolumeTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(VolumeTile.TILE_SPEC)
+    fun bindVolumeTile(volumeTile: VolumeTile): QSTileImpl<*>
 
     /** Inject WifiTile into tileMap in QSModule */
     @Binds
