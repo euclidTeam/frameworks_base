dee0e4a725ceab3880cb70694456315201619208
Danesh M <daneshm90@gmail.com>
2016-03-09 11:48:43 -0800

    Move high touch sensitivity and hovering to InputService
    Doing so allows us to keep track of user changes and restore
    preferences.
    
    This commit is squash of the following commits from CM 13.0:
    7348be747940afe7c3ec6a2b133b5473bc18573a Move high touch sensitivity and hovering to InputService
    f9a9d50491dc583ea568a4f12e57a2f97b9baacb InputMethodManager : Move registration to systemReady
    
    Since lineage-17.1 services/core/java/com/android/server/InputMethodManagerService.java is moved to
    services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
    
    Change-Id: I5a6af73129acefa6530ceb3f73cc4cd83a19a676
    Ticket-Id: CYNGNOS-1166

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6431,6 +6431,14 @@ public final class Settings {
         public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
 
         /**
+         * Whether the HighTouchSensitivity is activated or not.
+         * 0 = off, 1 = on
+         * @hide
+         */
+        public static final String HIGH_TOUCH_SENSITIVITY_ENABLE =
+                "high_touch_sensitivity_enable";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6643,6 +6651,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_PICTURE_ADJUSTMENT);
             PRIVATE_SETTINGS.add(LIVE_DISPLAY_HINTED);
             PRIVATE_SETTINGS.add(DISPLAY_ANTI_FLICKER);
+            PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
         }
 
         /**
@@ -12740,6 +12749,12 @@ public final class Settings {
          * @hide
          */
         public static final String VOLUME_PANEL_ON_LEFT = "volume_panel_on_left";
+
+       /**
+         * Whether touch hovering is enabled on supported hardware
+         * @hide
+         */
+        public static final String FEATURE_TOUCH_HOVERING = "feature_touch_hovering";
 
         /**
          * These entries are considered common between the personal and the managed profile,
