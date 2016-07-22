2d809a580ece98811630604b3bdb4ace536563b0
Sauhard Pande <sauhardp@codeaurora.org>
2016-07-21 18:23:21 -0700

    camera: Support exposing aux camera to apps
    * Replace vendor.camera.aux.packageblacklist with vendor.camera.aux.packageexcludelist
      as recommended by coding guidelines https://developers.google.com/style/word-list#blacklist.
    
    Original change:
    
    Author: Sauhard Pande <sauhardp@codeaurora.org>
    Date:   Thu Jul 21 18:23:21 2016 -0700
    
        Camera: Expose Aux camera to apps present in the whitelist
    
        Issue:
        3rd party apk cannot handle additional aux camera and may cause crash,
        Mono camera doesnt support all capabilities of HAL3 causing CTS issue.
    
        Fix:
        1. Expose aux camera to apps present in the whitelist
        2. ignore the availabe/unavailable status update for aux camera
        3. returning exception for open request for bad cameraid
    
        Change-Id: I15910154c6df205e6d4e00bfad30a00c9e3d5bee
        CRs-Fixed: 1086937
    
    ---------------------------------------------------------------------------
    
    Forward-ported to R and squashed with the following changes:
    
    Author: Rashed Abdel-Tawab <rashed@linux.com>
    Date:   Sat Mar 24 13:20:52 2018 -0400
    
        camera: Check if aux camera whitelist is set before restricting cameras
    
        Some devices have a fully featured 3rd camera and adding 20+ camera apps
        to the whitelist is impossible due to the string length limit on
        systemprops. Add a check to see if the prop is even set, and if not,
        check if the blacklist property is set and mark those apps to hide the
        3rd camera from. If a package is not part of the blacklist, just restore
        the original behaviour and expose all the cameras to the app.
    
        Change-Id: I6c3b33c077e8710c73b5d0fa28e1b017d6c43a58
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Fri Feb 8 08:42:33 2019 -0600
    
        Camera: Simplify code for Aux/Mono white and black lists
    
        Change-Id: I20c5454385d4d830a9a4d61926807a7eb1407c95
    
    Change-Id: I7eb9f78c37f06a4e713fe6bcbf1793052aad3583
    
    Change-Id: I15910154c6df205e6d4e00bfad30a00c9e3d5bee
    
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date: Thu, 12 Nov 2020 18:05:38 +0200
    
        CameraManager: Fixup exposing aux camera to apps
    
        Change-Id: Ic8c5844321a7310c2b72e1d7c2eceb5e3e8285e2
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -50,6 +50,7 @@ import android.os.Parcel;
 import android.os.Process;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Surface;
@@ -63,6 +64,7 @@ import com.android.internal.app.IAppOpsService;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Objects;
@@ -282,6 +284,23 @@ public class Camera {
     private static final int CAMERA_FACE_DETECTION_SW = 1;
 
     /**
+     * @hide
+     */
+    public static boolean shouldExposeAuxCamera() {
+        /**
+         * Force to expose only two cameras
+         * if the package name does not falls in this bucket
+         */
+        String packageName = ActivityThread.currentOpPackageName();
+        List<String> packageList = Arrays.asList(
+                SystemProperties.get("vendor.camera.aux.packagelist", packageName).split(","));
+        List<String> packageExcludelist = Arrays.asList(
+                SystemProperties.get("vendor.camera.aux.packageexcludelist", "").split(","));
+
+        return packageList.contains(packageName) && !packageExcludelist.contains(packageName);
+    }
+
+    /**
      * Returns the number of physical cameras available on this device.
      * The return value of this method might change dynamically if the device
      * supports external cameras and an external camera is connected or
@@ -312,8 +331,12 @@ public class Camera {
     public static int getNumberOfCameras(@NonNull Context context) {
         try (ScopedParcelState clientAttribution =
                 context.getAttributionSource().asScopedParcelState()) {
-            return _getNumberOfCameras(
+        int numberOfCameras = _getNumberOfCameras(
                     clientAttribution.getParcel(), getDevicePolicyFromContext(context));
+        if (!shouldExposeAuxCamera() && numberOfCameras > 2) {
+            numberOfCameras = 2;
+        }
+        return numberOfCameras;
         }
     }
 
@@ -328,6 +351,9 @@ public class Camera {
      *    low-level failure).
      */
     public static void getCameraInfo(int cameraId, CameraInfo cameraInfo) {
+        if (cameraId >= getNumberOfCameras()) {
+            throw new RuntimeException("Unknown camera ID");
+        }
         Context context = ActivityThread.currentApplication().getApplicationContext();
         final int rotationOverride = CameraManager.getRotationOverride(context);
         getCameraInfo(cameraId, context, rotationOverride, cameraInfo);
@@ -620,6 +646,9 @@ public class Camera {
 
     /** used by Camera#open, Camera#open(int) */
     Camera(int cameraId, @NonNull Context context, int rotationOverride) {
+        if (cameraId >= getNumberOfCameras()) {
+            throw new RuntimeException("Unknown camera ID");
+        }
         Objects.requireNonNull(context);
         final int err = cameraInit(cameraId, context, rotationOverride);
         if (checkInitErrors(err)) {
