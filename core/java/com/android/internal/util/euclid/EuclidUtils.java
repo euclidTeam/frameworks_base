f1fe1bcd927d6b2374ec73a71459e9b95256fcc2
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    base: [Utils] Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/util/euclid/EuclidUtils.java
+++ core/java/com/android/internal/util/euclid/EuclidUtils.java
@@ -43,6 +43,7 @@ import android.telephony.SubscriptionManager;
 import android.widget.Toast;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.content.om.IOverlayManager;
@@ -50,6 +51,8 @@ import android.content.om.OverlayInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.ResolveInfo;
 import android.content.pm.PackageManager;
+import android.os.BatteryManager;
+import android.content.pm.ResolveInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.database.ContentObserver;
@@ -151,6 +154,19 @@ public class EuclidUtils {
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
+    }
+
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
     }
 
     private static final String TAG = "EuclidUtils";
