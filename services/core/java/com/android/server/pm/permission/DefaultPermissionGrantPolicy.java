7c11327f529082b0255899f986b17da34998dad0
DennySPB <dennyspb@gmail.com>
2019-08-26 16:03:02 +0300

    Permissions: Grant Google Markup perms
    Grant runtime storage permissions.
    This fixes FC of google markup when using it along with Longshot
    
    Change-Id: I326a0ce9c0b7761552e7604a67bf4470045f6ab0

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -1112,6 +1112,9 @@ final class DefaultPermissionGrantPolicy {
         grantSystemFixedPermissionsToSystemPackage(pm,
                 getDefaultProviderAuthorityPackage("com.google.android.flipendo", userId),
                 userId, SUSPEND_APP_PERMISSIONS);
+
+        // Google Markup
+        grantSystemFixedPermissionsToSystemPackage(pm,"com.google.android.markup", userId, STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
