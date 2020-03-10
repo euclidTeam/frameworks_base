e48cd9bc4257174b84b2e3f3b8fbe79f797bdbd8
Marko Man <darkobas@gmail.com>
2020-03-10 18:34:12 +0000

    SystemUI: FPS Info Overlay & Tile
    @neobuddy89: Adapted tile for A13, A14.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
@@ -26,6 +26,7 @@ import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.CPUInfoTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.DcDimmingTile
+import com.android.systemui.qs.tiles.FPSInfoTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.LiveDisplayTile
@@ -102,6 +103,12 @@ interface EuclidOSModule {
     @IntoMap
     @StringKey(DcDimmingTile.TILE_SPEC)
     fun bindDcDimmingTile(dcDimmingTile: DcDimmingTile): QSTileImpl<*>
+
+    /** Inject FPSInfoTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(FPSInfoTile.TILE_SPEC)
+    fun FPSInfoTile(fpsInfoTile: FPSInfoTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds
