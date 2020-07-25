3aae9c084f7bae1886b9fa5b33bc077f74e53813
rituj <ritujbeniwal@gmail.com>
2020-07-25 15:18:03 +0530

    base: Introduce automated DC Dimming [1/3]
    * Schedule based on sunset/sunrise
    * Add QS Tile to control DC Dimming
    * Disable the service on devices with no DC dimming support
    * Set config_deviceDcDimmingSysfsNode to point to the relevant sysfs
    kernel node
    
    [ghostrider-reborn: adapt to 12, remove brightness-based schedule]
    
    Co-authored-by: Andrew Fluck <andrew@aospa.co>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Iaebf6624a8311722de3443c0ff5b033381749d20

--- packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.DataSwitchTile
+import com.android.systemui.qs.tiles.DcDimmingTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.SoundTile
@@ -75,6 +76,12 @@ interface EuclidOSModule {
     @IntoMap
     @StringKey(DataSwitchTile.TILE_SPEC)
     fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
+
+    /** Inject DcDimmingTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(DcDimmingTile.TILE_SPEC)
+    fun bindDcDimmingTile(dcDimmingTile: DcDimmingTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds
