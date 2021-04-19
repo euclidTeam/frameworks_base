1ca2af0161c23d82f07760710ef2f5f62e111455
Oliver Scott <olivercscott@gmail.com>
2021-04-19 14:48:01 -0400

    Add setting to allow/disallow interacting with quick settings on secure lockscreen (1/2)
    Do not handle tile clicks if keyguard is locked and setting is blocking
    
    Change-Id: I8194e9b7b64c0a23a42aeb3ffca90602f0882ec4
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13478,6 +13478,13 @@ public final class Settings {
                 "otp_redaction_lock_time";
 
         /**
+         * Whether user is allowed to interact with quick settings on lockscreen.
+         * @hide
+         */
+        public static final String QS_TILES_TOGGLEABLE_ON_LOCK_SCREEN =
+                "qs_tiles_toggleable_on_lock_screen";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
