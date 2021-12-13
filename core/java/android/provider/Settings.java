9bf8751f0da14f727e0efbc56b9376c127e2b752
Pranav Vashi <neobuddy89@gmail.com>
2021-12-13 08:34:09 +0530

    Battery Styles: Add customization for QS
    Original commit:
    https://github.com/crdroidandroid/android_frameworks_base/commit/d7c1672ef82e896b00d247484a65aab924bfe7a7
    
    Drop setting battery estimate mode as we dont ship Turbo with ROM
    and it conflicts with our setting
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I3c5d37c59a137e8fceadaebceab7d926485f0896
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6310,6 +6310,18 @@ public final class Settings {
          */
         public static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
 
+             /**
+         * @hide
+         */
+        public static final String QS_BATTERY_STYLE = "qs_battery_style";
+
+        /**
+         * @hide
+         */
+        public static final String QS_SHOW_BATTERY_PERCENT = "qs_show_battery_percent";
+
+
+
         /**
          * Integer property that specifes the color for screen flash notification as a
          * packed 32-bit color.
