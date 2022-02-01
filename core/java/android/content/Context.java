41b8f8c7a471996e79c759364a2ccc76188a285c
jhonboy121 <alfredmathew05@gmail.com>
2022-02-01 19:54:15 +0530

    base: Introduce app lock [1/3]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I19ed5ed8638776b0e93ecf8a308936030f9a9f92
    
    -----
    
    base: applock: refactor
    
    * encapsulate config data sources and add a data class for per app config
    * added support for migrating between different versions of data
    * fixed notification content not being redacted.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    -----
    
    services: applock: allow background authentication
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I1d05263f562d8ca4ac5d53c260b89814b399e0ea
    
    -----
    
    base: merge hidden app feature with applock
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Ie1966d856710353c849fa6bd26732dccea0ada85
    
    -----
    
    services: AppLockConfig: fix a derp in hidePackage()
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    -----
    
    services: applock: fix data migration and change secure notification parameter name
    
    * also fixed addPackage replacing data without checking if entry already exists
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    -----
    
    services: AppLockManagerService: use the og calling uid for target intent
    
    * fixes launching app activites not exported
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    -----
    
    AppLock: Allow locking all apps visible in launcher [1/2]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I920885d11aa9a74ca835f1d026d0834f9c851df9
    
    -----
    
    AppLock: Update API
    
    * To allow hiding apps in launcher even if app not protected
    
    Change-Id: I5e77b75be4d5aa0d137b04a15bc2f41cbc32a22d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    AppLock: Fix hidden apps reset after reboot
    
    Change-Id: I2dadc340f6a9c00aefe4ee9b8dc074a1c8aca717
    
    [aswin7469]
    - Adapted for 13 QPR3
    
    [Dhina17]
    - Adapted for android 14.
    
    [Pranav Vashi]
    - Add privapp permissions for Settings, updated for A14
    
    Change-Id: I906286e573fce744328edd29f11d0785c07c95a2
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    
    -----
    
    AppLock: Move services to new package
    
    Change-Id: Iffb96348d15e71e89a246326275f1b6f88b4e103
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    AppLock: Allow using face unlock for biometric authentication
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    applock: Handle null-safety errors for U QPR2
    
    Change-Id: Idc787c742afc79aca39c6a06d5326f280cc86dce
    
    -----
    
    AppLock: Make kotlin libs available only within this module
    
    It causes conflict with Bluetooth system service
    (from apex module) in runtime.
    
    Change-Id: Iaa13ce9ad252ac27ea7ca3133148ef572c5f71e7
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    SystemUI: biometrics: Follow user setting to confirm face unlock
    
    * It seems that AOSP code migration has some pending clean up
      which creates issue of not honoring setConfirmationRequired(false)
      for biometric prompt.
    * Let's disregard app setting and follow only user setting whether or
      not confirm face unlock prompt is required.
    
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I17281706bf17d24fd68442802b699952d1745db3
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -44,6 +44,7 @@ import android.annotation.UiContext;
 import android.annotation.UserIdInt;
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AppLockManager;
 import android.app.BroadcastOptions;
 import android.app.GameManager;
 import android.app.GrammaticalInflectionManager;
@@ -6602,6 +6603,14 @@ public abstract class Context {
      * @see com.oplus.os.LinearmotorVibrator
      */
     public static final String LINEARMOTOR_VIBRATOR_SERVICE = "linearmotor";
+
+    /**
+     * {@link AppLockManager}.
+     *
+     * @see #getSystemService(String)
+     * @hide
+     */
+    public static final String APP_LOCK_SERVICE = "app_lock";
 
     /**
      * Determine whether the given permission is allowed for a particular
