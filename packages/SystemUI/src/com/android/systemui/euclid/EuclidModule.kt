c34a6b22ad7a521068159570984baf2558a820d0
Dil3mm4 <dil3mm4.dev@gmail.com>
2022-05-05 00:31:24 +0530

    SystemUI: Add Autobrightness tile
    [johnmart19]
     - Rework for A12.1 environment
     - Launch Display Settings on long click
    
    [hridaya]
     - Fix building on A14 QPR3
    
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>
    Change-Id: Id395f0da3fb4997d835d135b01707adada153ecd

--- packages/SystemUI/src/com/android/systemui/euclid/EuclidModule.kt
+++ packages/SystemUI/src/com/android/systemui/euclid/EuclidModule.kt
@@ -19,6 +19,7 @@ package com.android.systemui.euclid
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
+import com.android.systemui.qs.tiles.AutoBrightnessTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.HeadsUpTile
@@ -88,4 +89,10 @@ interface EuclidModule {
     @IntoMap
     @StringKey(VpnTile.TILE_SPEC)
     fun bindVpnTile(vpnTile: VpnTile): QSTileImpl<*>
+
+    /** Inject AutoBrightnessTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(AutoBrightnessTile.TILE_SPEC)
+    fun bindAutoBrightnessTile(autobrightnessTile: AutoBrightnessTile): QSTileImpl<*>
 }
