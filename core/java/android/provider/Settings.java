acc79c5dab83d01055ce1a38b14bf7efb8be696c
timjosten <timjosten@gmail.com>
2022-06-07 12:44:25 +0900

    base: Remove IME space under keyboard feature [1/2]
    Change-Id: I7e322d85d8dbd937cf657d37d0a86bdc7f371c3f
    
    base: Improve IME space feature [1/2]
    
     ** AICP Edits: add backup validators
    
    Change-Id: Ic0c514850e3318077ad0964d0c0234ceccc86c83

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6538,6 +6538,14 @@ public final class Settings {
         public static final String GESTURE_NAVBAR_RADIUS = "gesture_navbar_radius";
 
         /**
+         * Gesture navbar IME space.
+         * Supported modes: 0 for normal length, 1 for narrow and 2 for hidden.
+         * Default 0.
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6771,6 +6779,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(HIGH_TOUCH_POLLING_RATE_ENABLE);
             PRIVATE_SETTINGS.add(HIGH_TOUCH_SENSITIVITY_ENABLE);
             PRIVATE_SETTINGS.add(NAV_BAR_COMPACT_LAYOUT);
+            PRIVATE_SETTINGS.add(NAVIGATION_BAR_IME_SPACE);
         }
 
         /**
