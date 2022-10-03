10d121ac4230595003bdcc29c27b7185b3f24c8d
tejasvp25 <tejasvp25@gmail.com>
2022-10-03 18:55:30 +0530

    ChangeReporter: Silence log spam
    Signed-off-by: tejasvp25 <tejasvp25@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/compat/ChangeReporter.java
+++ core/java/com/android/internal/compat/ChangeReporter.java
@@ -121,7 +121,6 @@ public class ChangeReporter {
      * @param state    of the reported change - enabled/disabled/only logged
      */
     public void reportChange(int uid, long changeId, int state) {
-        reportChange(uid, changeId, state, false, true);
     }
 
     /**
