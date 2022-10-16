987b8bbd86d0eaa6222b92d124b83dd87701f602
Pranav Vashi <neobuddy89@gmail.com>
2022-10-16 14:43:40 +0530

    SystemUI: Add Weather tile based on OmniJaws client
    * Initial code based on 11.0 commit https://github.com/crdroidandroid/android_frameworks_base/commit/97734230d0c1f92f5bc87ff8a515b198d92cdd15
    * Removed detailed adapter - deprecated now. :(
    * Simplify code and strings
    * Use location as tile label when enabled
    * handleClick will open settings when disabled, will open weather app when enabled
    * handleLongClick will always open settings
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
@@ -29,6 +29,7 @@ import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VpnTile
+import com.android.systemui.qs.tiles.WeatherTile
 import com.android.systemui.qs.tiles.WifiTile
 
 import dagger.Binds
@@ -110,6 +111,12 @@ interface EuclidOSModule {
     @IntoMap
     @StringKey(VpnTile.TILE_SPEC)
     fun bindVpnTile(vpnTile: VpnTile): QSTileImpl<*>
+
+    /** Inject WeatherTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(WeatherTile.TILE_SPEC)
+    fun bindWeatherTile(weatherTile: WeatherTile): QSTileImpl<*>
 
     /** Inject WifiTile into tileMap in QSModule */
     @Binds
