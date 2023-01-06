009e939e2eef4e95692a822d3cdb3009b11ce9ab
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-06 22:13:07 +0200

    base: Allow setting wake for notifications separately from doze [1/2]
    The strings were misleading. Disabling wake for notifications actually entirely disables doze.
    Add a new setting to configure wake for notification separately and amend doze switch strings accordingly
    
    Change-Id: I3b443953a2aef43c49c39144ea5ec52475e7e59d
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13453,6 +13453,12 @@ public final class Settings {
                 "otp_redaction_lock_time";
 
         /**
+         * Whether to trigger doze for new notifications
+         * @hide
+         */
+        public static final String DOZE_FOR_NOTIFICATIONS = "doze_for_notifications";
+
+        /**
          * Whether to show ambient instead of waking for the tap gesture
          * @hide
          */
