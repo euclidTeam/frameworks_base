c8800c5c6c8f9ab629de9ca06fdbf2b019988a29
Tobias Merkel <stylogey@gmail.com>
2023-06-22 18:52:41 +0200

    SystemUI: refresh system icons on theme change
    * Some usages of the system icons were not updated on a pack change.
      Change that by recreating the icons on theme changes.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/StatusBarIconControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/StatusBarIconControllerImpl.java
@@ -519,6 +519,11 @@ public class StatusBarIconControllerImpl implements Tunable,
         refreshIconGroups();
     }
 
+    @Override
+    public void onThemeChanged() {
+        refreshIconGroups();
+    }
+
     private String createExternalSlotName(String slot) {
         if (slot.endsWith(EXTERNAL_SLOT_SUFFIX)) {
             return slot;
