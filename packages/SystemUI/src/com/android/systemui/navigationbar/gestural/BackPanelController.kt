5f749387bfdedef1b3ac88c13d592894e7e1d8d3
Pranav Vashi <neobuddy89@gmail.com>
2023-11-01 23:22:02 +0530

    Allow to hide gesture on new back gesture affordance
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/BackPanelController.kt
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/BackPanelController.kt
@@ -155,6 +155,8 @@ constructor(
 
     internal val failsafeRunnable = Runnable { onFailsafe() }
 
+    private var backArrowVisibility = false
+
     internal enum class GestureState {
         /* Arrow is off the screen and invisible */
         GONE,
@@ -642,6 +644,10 @@ constructor(
         windowManager.addView(mView, layoutParams)
     }
 
+    override fun setBackArrowVisibility(enabled: Boolean) {
+        backArrowVisibility = enabled
+    }
+
     private fun isFlungAwayFromEdge(endX: Float, startX: Float = touchDeltaStartX): Boolean {
         val flingDistance = if (mView.isLeftPanel) endX - startX else startX - endX
         val flingVelocity =
@@ -895,7 +901,7 @@ constructor(
                 mView.isVisible = false
             }
             GestureState.ENTRY -> {
-                mView.isVisible = true
+                mView.isVisible = if (backArrowVisibility) true else false
 
                 updateRestingArrowDimens()
                 gestureEntryTime = systemClock.uptimeMillis()
