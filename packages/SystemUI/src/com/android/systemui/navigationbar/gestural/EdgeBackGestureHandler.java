22c85f4efd8b44c13df975096e46d075ef18551c
someone5678 <someone5678@users.noreply.github.com>
2024-02-11 17:10:14 +0900

    Gesture nav: Add back show recents action
    Change-Id: I2a487bd6752724840a018cbe2cb369f7d774f876
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1498,6 +1498,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 15: // Partial Screenshot
                 VoltageUtils.takeScreenshot(false);
                 break;
+            case 16: // Recents
+                VoltageUtils.sendKeycode(KeyEvent.KEYCODE_APP_SWITCH);
+                break;
         }
     }
 
