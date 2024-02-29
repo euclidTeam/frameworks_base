d40b17b6b2a8a1867aca575c348c8ce8f05d7240
Alex Yustasov <alex9yust@gmail.com>
2024-02-29 09:21:10 +0000

    SystemUI: Add anti flicker tile
    Change-Id: If2b23b509ff7f8c56abe427d8c35d27c453bd224
    
    [someone5678] Forward port to QPR3
    
    Change-Id: Ia5303e0f4307fc717f4b9f93e1fbb508e62f2eba
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    commit 7a64a1fa1c231b1493683c2b6459365338f3a8e4
    Author: Alex Yustasov <alex9yust@gmail.com>
    Date:   Tue Dec 13 14:20:56 2022 +0000
    
        QSTileHost: Fix crashes with tiles like Anti flicker
    
    Change-Id: I3245d842274cb1e42b745f9f7ca88389062eb9fe

--- packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
+++ packages/SystemUI/src/com/android/systemui/euclid/EuclidOSModule.kt
@@ -19,6 +19,7 @@ package com.android.systemui.euclid
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
+import com.android.systemui.qs.tiles.AntiFlickerTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CompassTile
@@ -57,6 +58,12 @@ interface EuclidOSModule {
     @IntoMap
     @StringKey(AODTile.TILE_SPEC)
     fun bindAODTile(aodTile: AODTile): QSTileImpl<*>
+
+    /** Inject AntiFlickerTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(AntiFlickerTile.TILE_SPEC)
+    fun bindAntiFlickerTile(antiFlickerTile: AntiFlickerTile): QSTileImpl<*>
 
     /** Inject CaffeineTile into tileMap in QSModule */
     @Binds
