f5e71c0ac51addaafa0048560365590bea09a643
someone5678 <someone5678@users.noreply.github.com>
2024-06-16 15:53:37 +0900

    fixup! Add Face Unlock with ParanoidSense
    Change-Id: I5819f424672179a3689f610e62da35e8a9cd03a5

--- services/core/java/com/android/server/biometrics/sensors/face/sense/SenseProvider.java
+++ services/core/java/com/android/server/biometrics/sensors/face/sense/SenseProvider.java
@@ -37,6 +37,7 @@ import android.hardware.biometrics.ITestSessionCallback;
 import android.hardware.biometrics.face.V1_0.IBiometricsFace;
 import android.hardware.face.Face;
 import android.hardware.face.FaceAuthenticateOptions;
+import android.hardware.face.FaceEnrollOptions;
 import android.hardware.face.FaceSensorPropertiesInternal;
 import android.hardware.face.IFaceServiceReceiver;
 import android.os.Binder;
@@ -382,7 +383,7 @@ public class SenseProvider implements ServiceProvider {
     }
 
     public SenseProvider(Context context, BiometricStateCallback biometricStateCallback, FaceSensorPropertiesInternal sensorProps, LockoutResetDispatcher lockoutResetDispatcher) {
-        this(context, biometricStateCallback, sensorProps, lockoutResetDispatcher, new BiometricScheduler(context, TAG, 0, null));
+        this(context, biometricStateCallback, sensorProps, lockoutResetDispatcher, new BiometricScheduler<>(context, 0, null));
     }
 
     private synchronized ISenseService getDaemon() {
@@ -553,7 +554,8 @@ public class SenseProvider implements ServiceProvider {
     public long scheduleEnroll(int sensorId, @NonNull IBinder token,
             @NonNull byte[] hardwareAuthToken, int userId, @NonNull IFaceServiceReceiver receiver,
             @NonNull String opPackageName, @NonNull int[] disabledFeatures,
-            @Nullable Surface previewSurface, boolean debugConsent) {
+            @Nullable Surface previewSurface, boolean debugConsent,
+            @NonNull FaceEnrollOptions options) {
         final long id = mRequestCounter.incrementAndGet();
         mHandler.post(() -> {
             if (getDaemon() == null) {
@@ -576,7 +578,9 @@ public class SenseProvider implements ServiceProvider {
                     ENROLL_TIMEOUT_SEC, previewSurface, mSensorId,
                     createLogger(BiometricsProtoEnums.ACTION_ENROLL,
                             BiometricsProtoEnums.CLIENT_UNKNOWN),
-                    mBiometricContext);
+                    mBiometricContext,
+                    mContext.getResources().getInteger(
+                                com.android.internal.R.integer.config_faceMaxTemplatesPerUser));
 
             mScheduler.scheduleClientMonitor(client, new ClientMonitorCallback() {
                 @Override
