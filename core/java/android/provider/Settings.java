3a4cd2960b06a9a51dea95776a38f7da2e00487a
someone5678 <someone5678@users.noreply.github.com>
2024-06-27 01:20:06 +0900

    fixup! base: Add support for window ignore secure [1/2]
    * Apply to all profiles
    
    Change-Id: I80539a8c4b1592ccfda6d00ab442a61192807355

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13320,13 +13320,6 @@ public final class Settings {
         public static final String EXTRA_AUTOMATIC_POWER_SAVE_MODE =
                 "extra_automatic_power_save_mode";
 
-	/**
-         * Control whether FLAG_SECURE is ignored for all windows.
-         * @hide
-         */
-        @Readable
-        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
-
         /**
          * Whether contextual screen timeout is enabled.
          *
@@ -20047,6 +20040,13 @@ public final class Settings {
          * @hide
          */
         public static final String RESTRICTED_NETWORKING_MODE = "restricted_networking_mode";
+
+	/**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
 
         /**
          * Setting indicating whether Low Power Standby is enabled, if supported.
