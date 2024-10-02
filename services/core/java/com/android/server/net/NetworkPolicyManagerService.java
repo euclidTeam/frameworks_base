2e909868b6ea44c1d4422e04cf9e64ff108db680
Tommy Webb <tommy@calyxinstitute.org>
2024-10-02 17:54:50 +0000

    Clear caller identity before updating restrictions
    Fix Android 15 regression in which apps with MANAGE_NETWORK_POLICY are
    not able to manipulate policies unless they are also AID_SYSTEM.
    
    Issue: calyxos#2733
    Issue: calyxos#2701
    Change-Id: I0536ec7ab412ea6102092d44c0c938a986cf2c9d
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/net/NetworkPolicyManagerService.java
+++ services/core/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -5761,9 +5761,11 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
             Trace.traceBegin(Trace.TRACE_TAG_NETWORK,
                     "updateRulesForDataUsageRestrictionsUL: " + uid);
         }
+        long token = Binder.clearCallingIdentity();
         try {
             updateRulesForDataUsageRestrictionsULInner(uid);
         } finally {
+            Binder.restoreCallingIdentity(token);
             Trace.traceEnd(Trace.TRACE_TAG_NETWORK);
         }
     }
