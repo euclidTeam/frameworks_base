4e43bc065c705c51abeca3319a35add5faf266fc
Pranav Vashi <neobuddy89@gmail.com>
2024-10-08 20:19:18 +0530

    fixup! ThemeOverlayController: Ensure we disable neutral overlay always
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/534
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -248,9 +248,6 @@ public class ThemeOverlayApplier implements Dumpable {
                 }
             }
 
-            transaction.setEnabled(getOverlayID(OVERLAY_BERRY_BLACK_THEME), blackMode, currentUser);
-            transaction.setEnabled(getOverlayID("android:neutral"), !blackMode, currentUser);
-
             try {
                 mOverlayManager.commit(transaction.build());
                 if (onComplete != null) {
@@ -260,7 +257,28 @@ public class ThemeOverlayApplier implements Dumpable {
             } catch (SecurityException | IllegalStateException e) {
                 Log.e(TAG, "setEnabled failed", e);
             }
+
+            checkDarkUserOverlays(currentUser, onComplete, blackMode);
         });
+    }
+
+    public void checkDarkUserOverlays(
+            int currentUser,
+            Runnable onComplete,
+            boolean blackMode
+    ) {
+        OverlayManagerTransaction.Builder transaction = getTransactionBuilder();
+        try {
+            transaction.setEnabled(getOverlayID(OVERLAY_BERRY_BLACK_THEME), blackMode, currentUser);
+            transaction.setEnabled(getOverlayID("android:neutral"), !blackMode, currentUser);
+            mOverlayManager.commit(transaction.build());
+            if (onComplete != null) {
+                Log.d(TAG, "Executing onComplete runnable");
+                mMainExecutor.execute(onComplete);
+            }
+        } catch (SecurityException | IllegalStateException e) {
+            Log.e(TAG, "setEnabled failed", e);
+        }
     }
 
     private OverlayIdentifier getOverlayID(String name) throws IllegalStateException {
