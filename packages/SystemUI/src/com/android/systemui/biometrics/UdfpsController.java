35c6f6282d17c08a4c4d08ff0da1c8c15530735d
Pranav Vashi <neobuddy89@gmail.com>
2024-11-09 18:49:18 +0530

    UdfpsAnimation: Address memory leaks and improvements
    Ref: https://github.com/crdroidandroid/issue_tracker/issues/478
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -820,21 +820,21 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
         mUdfpsVendorCode = mContext.getResources().getInteger(com.android.systemui.res.R.integer.config_udfpsVendorCode);
 
-        updateUdfpsAnimation();
-        mConfigurationController.addCallback(mConfigurationListener);
+        if (com.android.internal.util.euclid.EuclidUtils.isPackageInstalled(mContext,
+                "com.prism.settings.udfps.animations")) {
+            updateUdfpsAnimation();
+            mConfigurationController.addCallback(mConfigurationListener);
+        }
     }
 
     private void updateUdfpsAnimation() {
-        if (com.android.internal.util.euclid.EuclidUtils.isPackageInstalled(mContext,
-                "com.prism.settings.udfps.animations")) {
-            if (mUdfpsAnimation != null) {
-                mUdfpsAnimation.removeAnimation();
-                mUdfpsAnimation = null;
-            }
-            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController);
-            if (mUdfpsAnimation != null) {
-                mUdfpsAnimation.updatePosition();
-            }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.removeAnimation();
+            mUdfpsAnimation = null;
+        }
+        mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController);
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.updatePosition();
         }
     }
 
