d2fbc9912141803c69c5987488f215e3f063e8f9
Ido Ben-Hur <idoybh2@gmail.com>
2024-12-17 19:35:08 +0200

    fixup! base: Allow disabling turbulence noise after clicking the play button
    Change-Id: I8bde495e1627a9e6c27832bbc4581733dc8c7f7b

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
@@ -608,8 +608,7 @@ public class MediaControlPanel {
                         mLoadingEffect::finish,
                         TURBULENCE_NOISE_PLAY_DURATION
                 );
-            } else if (mContext.getResources().getBoolean(
-                    com.android.systemui.res.R.bool.config_turbulenceNoise)) {
+            } else {
                 mTurbulenceNoiseController.play(
                         Type.SIMPLEX_NOISE,
                         mTurbulenceNoiseAnimationConfig
@@ -1181,7 +1180,9 @@ public class MediaControlPanel {
     }
 
     private boolean shouldPlayTurbulenceNoise() {
-        return mButtonClicked && !mWasPlaying && isPlaying();
+        boolean isTurbulenceNoiseEnabled = mContext.getResources().getBoolean(
+                com.android.systemui.res.R.bool.config_turbulenceNoise);
+        return mButtonClicked && !mWasPlaying && isPlaying() && isTurbulenceNoiseEnabled;
     }
 
     private TurbulenceNoiseAnimationConfig createTurbulenceNoiseConfig() {
