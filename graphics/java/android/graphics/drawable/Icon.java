7e9e91170fcc03bdd7a1d56608ebbc6adf85a408
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-14 15:43:34 +0800

    Further reduce notifications bitmap resource usage
    * we're having qs panel lags when too many notifications with large bitmaps shows up (like youtube video preview), try compressing the notification bitmaps to webp to reduce memory usage by 40-60% assuming the bitmap is using jpg compression format
    
    Change-Id: Ie3c98a09abe5ec75081752c78d30ba6ac879c329

--- graphics/java/android/graphics/drawable/Icon.java
+++ graphics/java/android/graphics/drawable/Icon.java
@@ -1163,7 +1163,10 @@ public final class Icon implements Parcelable {
                     Math.max(1, scaledHeight), 
                     true /* filter */);
         }
-        return bitmap;
+        java.io.ByteArrayOutputStream byteArrayOutputStream = new java.io.ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.WEBP_LOSSY, 100, byteArrayOutputStream);
+        byte[] compressedBytes = byteArrayOutputStream.toByteArray();
+        return BitmapFactory.decodeByteArray(compressedBytes, 0, compressedBytes.length);
     }
 
     /**
