98f190bacb78fcca34398daa730fee0e19d02bf5
703joko <703joko@gmail.com>
2025-03-25 19:18:37 +0800

    Battery Styles: Allow setting percent view on left [1/2]
    neobuddy89:
    * Adapt to our setup
    * Do not add separate tunable
    
    Change-Id: I98db159a718f0229a95a46e9c714ce6f2a5e0f90
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -587,7 +587,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         final boolean drawPercentInside = mShowPercentMode == MODE_DEFAULT &&
                 showBatteryPercent == 1;
         final boolean drawPercentOnly = mShowPercentMode == MODE_ESTIMATE ||
-                showBatteryPercent == 2;
+                showBatteryPercent >= 2;
         boolean shouldShow =
                 (drawPercentOnly && (!drawPercentInside || isCharging()) ||
                 getBatteryStyle() == BATTERY_STYLE_TEXT);
@@ -606,6 +606,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
                 Resources res = getContext().getResources();
                 mBatteryPercentView.setPaddingRelative(
                         res.getDimensionPixelSize(R.dimen.battery_level_padding_start), 0, 0, 0);
+                setLayoutDirection(showBatteryPercent > 2 ? View.LAYOUT_DIRECTION_RTL : View.LAYOUT_DIRECTION_LTR);
             }
         } else {
             mAccessorizedDrawable.showPercent(drawPercentInside);
