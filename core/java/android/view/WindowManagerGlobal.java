76dddc6e0e09073e9e603b78b751a46fe2455753
lijilou <lijilou@xiaomi.corp-partner.google.com>
2025-04-17 16:51:42 +0800

    Remove unnecessary non-empty judgment in WindowManagerGlobal file.
    Bug: none
    Flag: EXEMPT minor optimization
    Google: 3594790
    Change-Id: I1731acc34acee364d5e2eaea293da98fe27b7632
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/view/WindowManagerGlobal.java
+++ core/java/android/view/WindowManagerGlobal.java
@@ -587,9 +587,7 @@ public final class WindowManagerGlobal {
         ViewRootImpl root = mRoots.get(index);
         View view = root.getView();
 
-        if (root != null) {
-            root.getImeFocusController().onWindowDismissed();
-        }
+        root.getImeFocusController().onWindowDismissed();
         boolean deferred = root.die(immediate);
         if (view != null) {
             view.assignParent(null);
