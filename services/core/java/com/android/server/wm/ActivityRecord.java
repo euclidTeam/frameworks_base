2024de507e8e325a582925b85fb828d68171aa21
lijilou <lijilou@xiaomi.corp-partner.google.com>
2025-04-18 14:33:46 +0800

    Removed unnecessary global lock for register/unregister capture observer method.
    The registerScreenCaptureObserver and unregisterScreenCaptureObserver method have added the wms global lock,we do not need add lock in this two methods.
    
    Bug: none
    Flag: EXEMPT minor optimization
    Google: 3596550
    Change-Id: I5db6342fa1a34f225200296d54539c36501b8d73
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -6883,7 +6883,7 @@ final class ActivityRecord extends WindowToken {
         }
     }
 
-    public void reportScreenCaptured() {
+    void reportScreenCaptured() {
         if (mCaptureCallbacks != null) {
             final int n = mCaptureCallbacks.beginBroadcast();
             for (int i = 0; i < n; i++) {
@@ -6897,20 +6897,16 @@ final class ActivityRecord extends WindowToken {
         }
     }
 
-    public void registerCaptureObserver(IScreenCaptureObserver observer) {
-        synchronized (mWmService.mGlobalLock) {
-            if (mCaptureCallbacks == null) {
-                mCaptureCallbacks = new RemoteCallbackList<IScreenCaptureObserver>();
-            }
-            mCaptureCallbacks.register(observer);
+    void registerCaptureObserver(IScreenCaptureObserver observer) {
+        if (mCaptureCallbacks == null) {
+            mCaptureCallbacks = new RemoteCallbackList<IScreenCaptureObserver>();
         }
+        mCaptureCallbacks.register(observer);
     }
 
-    public void unregisterCaptureObserver(IScreenCaptureObserver observer) {
-        synchronized (mWmService.mGlobalLock) {
-            if (mCaptureCallbacks != null) {
-                mCaptureCallbacks.unregister(observer);
-            }
+    void unregisterCaptureObserver(IScreenCaptureObserver observer) {
+        if (mCaptureCallbacks != null) {
+            mCaptureCallbacks.unregister(observer);
         }
     }
 
