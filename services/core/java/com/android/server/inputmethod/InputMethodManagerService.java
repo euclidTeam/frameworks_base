2714dc70899eee97b9793ab89d80b06f2a0654e4
Michael Bestas <mkbestas@lineageos.org>
2025-04-23 22:07:55 +0300

    fixup! Introduce high touch polling rate feature control
    Change-Id: Ia93a250017e8f8b5991b929747953c00033983c1
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
+++ services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
@@ -3093,8 +3093,10 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
         if (!mLineageHardware.isSupported(LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE)) {
             return;
         }
-        final boolean enabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HIGH_TOUCH_POLLING_RATE_ENABLE, 0) == 1;
+        // If user is a profile, use preference of its parent profile.
+        final int profileParentUserId = mUserManagerInternal.getProfileParentId(mCurrentImeUserId);
+        final boolean enabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.HIGH_TOUCH_POLLING_RATE_ENABLE, 0, profileParentUserId) == 1;
         mLineageHardware.set(LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE, enabled);
     }
 
