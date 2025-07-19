2ad94f005051bfcd19181bd5a85b7dfb67515092
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-19 17:11:37 +0800

    [FEATURE] add always-on-display auto scheduling
    Change-Id: I6b87aa5406df0b4e6c8b20f51accb1c60df169d9
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2838,6 +2838,12 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
             t.traceEnd();
+
+            if (context.getResources().getBoolean(R.bool.config_dozeAlwaysOnDisplayAvailable)) {
+                t.traceBegin("AODScheduleService");
+                mSystemServiceManager.startService(com.android.server.display.AODScheduleService.class);
+                t.traceEnd();
+            }
         }
 
         t.traceBegin("StartMediaProjectionManager");
