4eab6b69d31e5745079ca40ea51ed7c548e162d8
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-31 18:45:53 +0800

    [BUGFIX] fix dt2s headsup conflict
    Change-Id: I52312c3d00b19d51f7cc48d8e66c86d501fbbadb
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -4078,7 +4078,8 @@ public final class NotificationPanelViewController implements
                 return false;
             }
 
-            if (mDoubleTapToSleepEnabled && !mPulsing && !mDozing) {
+            boolean headsUpVisible = mHeadsUpAnimatingAway || mHeadsUpPinnedMode;
+            if (mDoubleTapToSleepEnabled && !mPulsing && !mDozing && !headsUpVisible) {
                 mDoubleTapGesture.onTouchEvent(event);
             }
 
