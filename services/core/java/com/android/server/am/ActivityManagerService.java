e151704e9d8401c65d6a93fabb78c9c869cf36af
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-05 17:53:14 +0800

    [BOOST FRAMEWORK] skip process with no threads
    Change-Id: I693c4f10e7862d9e1cb457f9f359c0a214539092
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19774,7 +19774,8 @@ public class ActivityManagerService extends IActivityManager.Stub
             }
 
             ProcessRecord topAppProc = getProcessRecord(topApp);
-            if (topAppProc == null) {
+            if (topAppProc == null || topAppProc.getThread() == null) {
+                Slog.w(TAG, "Invalid top app process, is null or not attached to a thread: " + topAppProc);
                 return;
             }
 
