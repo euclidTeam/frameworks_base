69d2c583262a0df532d1a945f018129eaa4cc73f
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-08 19:38:14 +0800

    [BUGFIX] fix footer actions theme
    Change-Id: I6ea64be95441c5ce26b5a2e68deb918f04d0c3d7
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
+++ packages/SystemUI/src/com/android/systemui/qs/composefragment/QSFragmentCompose.kt
@@ -144,7 +144,6 @@ import com.android.systemui.qs.panels.ui.compose.TileGrid
 import com.android.systemui.qs.shared.ui.ElementKeys
 import com.android.systemui.qs.ui.composable.QuickSettingsShade
 import com.android.systemui.qs.ui.composable.QuickSettingsShade.systemGestureExclusionInShade
-import com.android.systemui.qs.ui.composable.QuickSettingsTheme
 import com.android.systemui.res.R
 import com.android.systemui.util.LifecycleFragment
 import com.android.systemui.util.animation.UniqueObjectHostView
@@ -832,7 +831,7 @@ constructor(
                         }
                     }
                 }
-                QuickSettingsTheme {
+                PlatformTheme(isDarkTheme = isSystemInDarkTheme()) {
                     Element(
                         ElementKeys.FooterActions,
                         Modifier.sysuiResTag(ResIdTags.qsFooterActions),
