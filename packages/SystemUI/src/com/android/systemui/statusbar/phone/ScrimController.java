b120417c485ed9c76375571f20b2afd1d5053080
Dmitrii <bankersenator@gmail.com>
2025-08-14 17:19:23 +0000

    ScrimController: Prevent NPE on theme change before views are attached
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -1170,6 +1170,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
 
         if (mScrimBehind != null) {
             dispatchBackScrimState(mScrimBehind.getViewAlpha());
+            com.android.systemui.util.ScrimUtils.getInstance(mScrimBehind.getContext()).onScrimDispatched();
         }
     }
 
