3bd0b3593a40cd114881291b980bae4ea9f1120b
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-17 07:39:28 +0800

    [BOOST FRAMEWORK] skip camera cpuset
    Change-Id: I7329b55b9389ff60955ab2bc1e738c2dcb0fd014
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19706,11 +19706,8 @@ public class ActivityManagerService extends IActivityManager.Stub
                 cpuset = bgCpuset;
                 break;
             case "cam":
-                path = "/dev/cpuset/camera-daemon/cpus";
-                cpuset = bgCpuset;
-                break;
             default:
-                Log.w("adjustCpusetCpus", "Unknown cgroup: " + cgroup);
+                Log.w("adjustCpusetCpus", "Skipping adjustment");
                 return;
         }
 
