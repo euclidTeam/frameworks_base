de39318f51d76c9bbb376cbf47128cc2c3004b77
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-21 18:19:58 +0800

    [PERF] boost display cpuset
    Change-Id: I12f373b32bcb85f8933dd1a376ac8b7b219d4ad1
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19774,12 +19774,15 @@ public class ActivityManagerService extends IActivityManager.Stub
         private static final String ROOT_CGROUP_PROCS = "/dev/cpuctl/cgroup.procs";
         private static final String RESTRICTED_UCLAMP_MAX = "/dev/cpuctl/restricted/cpu.uclamp.max";
         private static final String RESTRICTED_UCLAMP_MIN = "/dev/cpuctl/restricted/cpu.uclamp.min";
+        private static final String DISPLAY_UCLAMP_MAX = "/dev/cpuctl/display/cpu.uclamp.max";
+        private static final String DISPLAY_UCLAMP_MIN = "/dev/cpuctl/display/cpu.uclamp.min";
         private static final String BG_CPUSET = SystemProperties.get("persist.sys.euclid_cpu_bg", "0-3");
         private static final String NT_FG_CPUSET = SystemProperties.get("persist.sys.euclid_cpu_unlimit_ui", "0-7");
         private static final String BG_LIMIT = SystemProperties.get("persist.sys.euclid_cpu_limit_bg", "0-1");
         private static final String FG_LIMIT = SystemProperties.get("persist.sys.euclid_cpu_limit_ui", "0-2");
         private static final String ALL_CORES = SystemProperties.get("persist.sys.euclid_cpu_unlimit_ui", "0-7");
         private static final String BIG_CORES = getCpuRange(SystemProperties.get("persist.sys.euclid_cpu_big", "4,5,6,7"));
+        private static final String SF_UCLAMP_MIN_BOOST = SystemProperties.get("ro.surface_flinger.uclamp.min", "165");
 
         private int mTopAppPid = -1;
         private String currentReason = "";
@@ -19887,7 +19890,10 @@ public class ActivityManagerService extends IActivityManager.Stub
                 FileUtils.stringToFile(enable ? RESTRICTED_CGROUP_PROCS : ROOT_CGROUP_PROCS, String.valueOf(pid));
                 FileUtils.stringToFile(RESTRICTED_UCLAMP_MIN, enable ? "100" : "0");
                 FileUtils.stringToFile(RESTRICTED_UCLAMP_MAX, "100");
+                FileUtils.stringToFile(DISPLAY_UCLAMP_MIN, enable ? SF_UCLAMP_MIN_BOOST : "0");
+                FileUtils.stringToFile(DISPLAY_UCLAMP_MAX, "293");
                 executeAdjustCpusetCpus("/dev/cpuset/restricted/cpus", enable ? BIG_CORES : ALL_CORES);
+                executeAdjustCpusetCpus("/dev/cpuset/display/cpus", enable ? BIG_CORES : ALL_CORES);
             } catch (Exception e) {
                 Slog.w(TAG, "Failed to " + (enable ? "enable" : "disable") + " restricted boost: " + e);
             }
