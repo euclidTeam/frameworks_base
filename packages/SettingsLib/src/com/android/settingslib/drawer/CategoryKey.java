8ed08de181a9f07e3088125a9c37c34ca6f42225
Pranav Vashi <neobuddy89@gmail.com>
2025-09-27 09:09:53 +0000

    base: Add metric for Prism Co-authored-by: Bauuuuu <abansode@outlook.com> Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -24,6 +24,7 @@ public final class CategoryKey {
     public static final String CATEGORY_HOMEPAGE = "com.android.settings.category.ia.homepage";
 
     // Top level category.
+    public static final String CATEGORY_PRISM = "com.android.settings.category.ia.prism";
     public static final String CATEGORY_NETWORK = "com.android.settings.category.ia.wireless";
     public static final String CATEGORY_CONNECT = "com.android.settings.category.ia.connect";
     public static final String CATEGORY_DEVICE = "com.android.settings.category.ia.device";
