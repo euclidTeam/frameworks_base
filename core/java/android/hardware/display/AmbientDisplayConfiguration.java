1415187cbfb04aecd045f47a48052ba59413aa83
Mrick343 <mrinmoy8429de@gmail.com>
2025-10-07 21:24:33 +0530

    Revert "base: Allow setting wake for notifications separately from doze [1/2]"
    This reverts commit 009e939e2eef4e95692a822d3cdb3009b11ce9ab.

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -55,8 +55,7 @@ public class AmbientDisplayConfiguration {
             Settings.Secure.DOZE_DOUBLE_TAP_GESTURE,
             Settings.Secure.DOZE_WAKE_LOCK_SCREEN_GESTURE,
             Settings.Secure.DOZE_WAKE_DISPLAY_GESTURE,
-            Settings.Secure.DOZE_TAP_SCREEN_GESTURE,
-            Settings.Secure.DOZE_FOR_NOTIFICATIONS
+            Settings.Secure.DOZE_TAP_SCREEN_GESTURE
     };
 
     /** Non-user configurable doze settings */
@@ -108,12 +107,6 @@ public class AmbientDisplayConfiguration {
     private boolean pulseOnCustomDozeEventEnabled(int user) {
         return (Settings.System.getInt(mContext.getContentResolver(), Settings.System.DOZE_TRIGGER_DOUBLETAP, 0) != 0)
                 && pulseOnNotificationAvailable();
-    }
-
-    /** @hide */
-    public boolean userPulseOnNotificationEnabled(int user) {
-        return boolSettingDefaultOn(Settings.Secure.DOZE_FOR_NOTIFICATIONS, user)
-                && pulseOnNotificationEnabled(user);
     }
 
     /** @hide */
