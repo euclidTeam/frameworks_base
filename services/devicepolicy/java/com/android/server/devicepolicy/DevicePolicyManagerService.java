6aa60d95cf6c2a1d659e134b2464616e6fdcfcf2
Mrick343 <mrinmoy8429de@gmail.com>
2025-10-22 13:29:18 +0000

    Revert "Support logging out secondary users"
    This reverts commit 3f579031a66768a396de21c8f1b1acfffb40ad4e.

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -12971,11 +12971,7 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
 
     private @UserIdInt int getLogoutUserIdUnchecked() {
         synchronized (getLockObject()) {
-            final int logoutUserId = mLogoutUserId;
-            if (logoutUserId == UserHandle.USER_NULL) {
-                return UserHandle.USER_SYSTEM;
-            }
-            return logoutUserId;
+            return mLogoutUserId;
         }
     }
 
